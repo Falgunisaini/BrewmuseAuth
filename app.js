07ab6b8076fe4db471d485ec54ae4f989d45093c
FalguniSaini <falgunisaini675@gmail.com>
2022-05-28 00:57:22 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express =require('express');
 const app=express();
 const db = require('./db');
-// const dotenv=require("dotenv");
-// dotenv.config();
 const port=process.env.PORT || 7800;
 const cors =require('cors');
-// app.use(express.urlencoded({ extended: false }))
-// app.set('view engine', 'ejs')
+
 app.use(cors())
 const AuthController= require('./controller/authController');
 app.use('/api/auth',AuthController);
--- controller/authController.js
+++ controller/authController.js
@@ -1,55 +1,54 @@
-const express =require('express');
-const router=express.Router();
-const bodyParser =require('body-parser');
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
 
-const jwt = require ("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const config  =require("../config");
-const User =require("../modal/userSchema");
-router.use(bodyParser.urlencoded({extended:true}));
+const config = require("../config");
+const User = require("../modal/userSchema");
+router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-// router.use(express.urlencoded({ extended: false }))
-// router.set('view engine', 'ejs')
-//To get all the user
-router.get('/users',(req,res)=>{
-    User.find({},(err,data)=>{
-        if(err) throw err;
+
+//To get all the users
+router.get('/users', (req, res) => {
+    User.find({}, (err, data) => {
+        if (err) throw err;
         res.send(data)
     })
 })
 
 //Register
-router.post('/register',(req,res)=>{
-    let hashPassword=bcrypt.hashSync(req.body.password,8);
+router.post('/register', (req, res) => {
+    let hashPassword = bcrypt.hashSync(req.body.password, 8);
     User.create({
-        name:req.body.name,
-        email:req.body.email,
-        password:hashPassword,
-        phone:req.body.phone,
-        role:req.body.role?req.body.role:"User",
-        
-    },(err,data)=>{
-        if(err)  throw err;
+        name: req.body.name,
+        email: req.body.email,
+        password: hashPassword,
+        phone: req.body.phone,
+        role: req.body.role ? req.body.role : "User",
+
+    }, (err, data) => {
+        if (err) throw err;
         res.status(200).send("Registration SuccessFull")
-    } )
+    })
 
 })
 
 //Login
-router.post('/login',(req,res)=>{
-    User.findOne({email:req.body.email},(err,user)=>{
-        if(err){
-            return res.status(500).send({auth:false,token:"Error while Login"})
+router.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) {
+            return res.status(500).send({ auth: false, token: "Error while Login" })
         }
-        if(!user){
-            return res.status(200).send({auth:false,token:"No user Found Register First"})
-        }else{
-            const passIsValid=bcrypt.compareSync(req.body.password,user.password)
-            if(!passIsValid){
-                return res.status(200).send({auth:false,token:"Wrong Password"})
-            }else{
-                let token=jwt.sign({id:user._id},config.secret,{expiresIn:86400})
-                res.status(200).send({auth:true,token:token})
+        if (!user) {
+            return res.status(200).send({ auth: false, token: "No user Found Register First" })
+        } else {
+            const passIsValid = bcrypt.compareSync(req.body.password, user.password)
+            if (!passIsValid) {
+                return res.status(200).send({ auth: false, token: "Wrong Password" })
+            } else {
+                let token = jwt.sign({ id: user._id }, config.secret, { expiresIn: 86400 })
+                res.status(200).send({ auth: true, token: token })
             }
 
         }
@@ -57,57 +56,57 @@ router.post('/login',(req,res)=>{
 })
 
 //userInfo
-router.get("/userInfo",(req,res)=>{
-    let token=req.headers['x-access-token'];
-    if(!token){
-        res.send({auth:false,token:"No token Provided"})
+router.get("/userInfo", (req, res) => {
+    let token = req.headers['x-access-token'];
+    if (!token) {
+        res.send({ auth: false, token: "No token Provided" })
     }
-    jwt.verify(token,config.secret,(err,user)=>{
-        if(err) return res.status(200).send({auth:false,token:"Invalid Token"})
-        User.findById(user.id,(err,result)=>{
-            if(err) throw err;
+    jwt.verify(token, config.secret, (err, user) => {
+        if (err) return res.status(200).send({ auth: false, token: "Invalid Token" })
+        User.findById(user.id, (err, result) => {
+            if (err) throw err;
             res.send(result)
         })
     })
 })
 
 //deleteUser
-router.delete("/delete",(req,res)=>{
-    User.remove({},(err,data)=>{
-        if(err) throw err;
+router.delete("/delete", (req, res) => {
+    User.remove({}, (err, data) => {
+        if (err) throw err;
         res.send("User Deleted")
     })
 })
 
-router.put("/forgot",(req,res)=>{
-    let email=req.body.email;
-    let password=req.body.password;
-   User.findOne({email},(err,user)=>{
-       if(err){
-         return  res.status(500).send({auth:false,token:"Error"})
-       }
-       if(!user){
-          return  res.status(200).send({auth:false,token:"User Not Found Register First"})
-       }else{
-          const newPassword=bcrypt.hashSync(password,8);
-          user.updateOne(
-              {
-                             $set:{
-                                 "password":newPassword
-                             }
-                         },(err,result)=>{
-                             if(err) throw err;
-                             res.status(200).send({auth:true,token:"Password Reset Successfully",result})
-                         }
-              
-          )
-      
-  
-          
-          
-       }
-  
-   })
-  })
+router.put("/forgot", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return res.status(500).send({ auth: false, token: "Error" })
+        }
+        if (!user) {
+            return res.status(200).send({ auth: false, token: "User Not Found Register First" })
+        } else {
+            const newPassword = bcrypt.hashSync(password, 8);
+            user.updateOne(
+                {
+                    $set: {
+                        "password": newPassword
+                    }
+                }, (err, result) => {
+                    if (err) throw err;
+                    res.status(200).send({ auth: true, token: "Password Reset Successfully", result })
+                }
 
-module.exports=router;
+            )
+
+
+
+
+        }
+
+    })
+})
+
+module.exports = router;
